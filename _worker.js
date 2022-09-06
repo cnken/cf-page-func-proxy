f441f306992bc58dd7f7fb31e4baf912a00e4a7d
cnken <cnken@users.noreply.github.com>
2022-09-06 23:53:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="cf.imageoss.com";
+        url.hostname="65.108.198.243.sslip.io:443";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
