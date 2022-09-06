45455792866257084aa6444e6a6e55edd8d903a3
cnken <cnken@users.noreply.github.com>
2022-09-07 00:02:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="65.108.198.243";
+        url.hostname="65.108.198.243:80";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
